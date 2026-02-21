1d3cb1fb7f27f51abb4282b6e676b90f4475091d
voldemortka <samurajka37@gmail.com>
2026-02-21 22:31:05 +0100

    first ideas, what to put, prep

---
+++ main.js
@@ -0,0 +1,28 @@
+document.head.innerHTML += `
+    <meta charset='utf-8'/>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta hettp-equiva='X-UA-Compatible' content='IE-edge.chrome=1' />
+
+    <!--To będzie taka tarta okrągła PIE XD Wpis tam gdzieś liczbę PI XD-->
+    <link rel='website icon' href='logo.png' type='png'/>
+
+    <meta name="application-name" content="ReciPIE">
+    <meta name="apple-mobile-web-app-title" content="ReciPIE">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="logo.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="logo.png">
+    <link rel="apple-touch-icon" href="logo.png">
+
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="theme-color" content="#EEDDC4"> <!--Coś tu wymyśl wgl-->
+
+    <link rel='stylesheet' href='css/fontello.css' type='text/css'/>
+    <link rel='stylesheet' href='style.css' type='text/css'/>
+
+    <script src="main.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+s    `;
